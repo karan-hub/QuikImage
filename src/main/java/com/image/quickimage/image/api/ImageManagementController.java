0fab01df28ffdef47e025e7ffdfb1ca513429525
karan-hub <karanc4143@gmail.com>
2026-02-24 06:42:09 +0530

    we have built successuly  now its time to  build froent end

--- src/main/java/com/image/quickimage/image/api/ImageManagementController.java
+++ src/main/java/com/image/quickimage/image/api/ImageManagementController.java
@@ -6,6 +6,7 @@ import com.image.quickimage.image.repository.ImageRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/management")
 @AllArgsConstructor
+@CrossOrigin(origins = "*")
 public class ImageManagementController {
 
     private final ImageRepository repository;
